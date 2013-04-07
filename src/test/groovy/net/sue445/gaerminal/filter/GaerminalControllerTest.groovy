e1bcd7b4b4d7fd4d5afc42da78017a2f5678b3ea
sue445 <sue445@sue445.net>
2013-04-07 23:56:33 +0900

    use slim3
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    groovy -> java
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP
    
    WIP

--- src/test/groovy/net/sue445/gaerminal/filter/GaerminalControllerTest.groovy
+++ src/test/groovy/net/sue445/gaerminal/filter/GaerminalControllerTest.groovy
@@ -1,15 +1,15 @@
 package net.sue445.gaerminal.filter
-
 import org.junit.Test
 import org.junit.experimental.runners.Enclosed
 import org.junit.runner.RunWith
+import org.slim3.tester.ControllerTestCase
 
 import javax.servlet.FilterConfig
 
 @RunWith(Enclosed)
 class GaerminalControllerTest {
 
-    static class init {
+    static class init extends ControllerTestCase {
         GaerminalController filter = new GaerminalController()
 
         @Test
@@ -35,6 +35,17 @@ class GaerminalControllerTest {
 
             // assertion
             assert filter.pathPrefix == pathPrefix
+        }
+    }
+
+    static class doFilter extends ControllerTestCase  {
+        GaerminalController filter = new GaerminalController()
+
+        @Test
+        void index(){
+            filter.doFilter(tester.request, tester.response, tester.filterChain)
+
+            assert tester.response.getOutputAsString().contains("helloworld") == true
         }
     }
 }
