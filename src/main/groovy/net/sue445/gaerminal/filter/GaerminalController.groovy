c73e1a3e205de340dca27146af900ee2b6da3486
sue445 <sue445@sue445.net>
2013-04-13 16:32:39 +0900

    impl GaerminalController
    refactor test
    
    WIP
    
    WIP
    
    java -> groovy
    
    run gaeRun
    
    impl 404
    
    support index
    
    refactor
    
    WIP

---
+++ src/main/groovy/net/sue445/gaerminal/filter/GaerminalController.groovy
@@ -0,0 +1,64 @@
+package net.sue445.gaerminal.filter
+
+import net.sue445.gaerminal.util.FileUtil
+
+import javax.servlet.*
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+public class GaerminalController implements Filter {
+    private static final String DEFAULT_PATH_PREFIX = "/gaerminal/"
+
+    protected String pathPrefix = DEFAULT_PATH_PREFIX
+
+    @Override
+    public void init(FilterConfig config)  {
+        String pathPrefix = config.getInitParameter("pathPrefix")
+
+        if(!isEmpty(pathPrefix)){
+            if(!pathPrefix.endsWith("/")){
+                pathPrefix += "/"
+            }
+            this.pathPrefix = pathPrefix
+        }
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) {
+        doFilter((HttpServletRequest)request, (HttpServletResponse)response, chain)
+    }
+
+    protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) {
+        if(!request.getServletPath().startsWith(pathPrefix)){
+            chain.doFilter(request, response)
+            return
+        }
+
+        String pageName = request.getServletPath().replaceFirst(pathPrefix, "")
+        if(pageName.isEmpty()){
+            pageName = "index"
+        }
+
+        String content = FileUtil.readPage(pageName)
+        if(content == null) {
+            content = "Not Found"
+            response.setStatus(404)
+            response.setContentType("text/plain; charset=UTF-8")
+
+        } else {
+            response.setStatus(200)
+            response.setContentType("text/html; charset=UTF-8")
+        }
+
+        response.setContentLength(content.length())
+        response.getOutputStream().print(content)
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+    boolean isEmpty(String str){
+        return str == null || str.isEmpty()
+    }
+}
--- src/test/groovy/net/sue445/gaerminal/filter/GaerminalControllerTest.groovy
+++ src/test/groovy/net/sue445/gaerminal/filter/GaerminalControllerTest.groovy
@@ -8,7 +8,6 @@ import org.slim3.tester.ControllerTestCase
 
 import javax.servlet.FilterConfig
 
-@RunWith(Enclosed)
 class GaerminalControllerTest {
 
     static class init extends ControllerTestCase {
@@ -43,12 +42,43 @@ class GaerminalControllerTest {
     static class doFilter extends ControllerTestCase  {
         GaerminalController filter = new GaerminalController()
 
-        @Ignore
         @Test
-        void index(){
+        void "when exists file, return page file"(){
+            // setup
+            tester.request.setServletPath(GaerminalController.DEFAULT_PATH_PREFIX + "helloworld")
+
+            // exercise
             filter.doFilter(tester.request, tester.response, tester.filterChain)
 
-            assert tester.response.getOutputAsString().contains("helloworld") == true
+            // assertion
+            assert tester.response.getOutputAsString().contains("Hello World") == true
+            assert tester.response.getStatus() == 200
+        }
+
+        @Test
+        void "when not exists file, return status 404"(){
+            // setup
+            tester.request.setServletPath(GaerminalController.DEFAULT_PATH_PREFIX + "invalid")
+
+            // exercise
+            filter.doFilter(tester.request, tester.response, tester.filterChain)
+
+            // assertion
+            assert tester.response.getOutputAsString() == "Not Found"
+            assert tester.response.getStatus() == 404
+        }
+
+        @Test
+        void "when called /gaerminal/, return index.html"(){
+            // setup
+            tester.request.setServletPath(GaerminalController.DEFAULT_PATH_PREFIX)
+
+            // exercise
+            filter.doFilter(tester.request, tester.response, tester.filterChain)
+
+            // assertion
+            assert tester.response.getOutputAsString().contains("pages/index.html") == true
+            assert tester.response.getStatus() == 200
         }
     }
 }
